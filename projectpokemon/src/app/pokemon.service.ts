dffaffdfac2bcdec88ed0644ed94452be518d646
teddy <ventialaw@hotmail.fr>
2017-10-01 14:49:40 +0200

    Use Promise to get async data from PokeApi...so slow

--- projectpokemon/src/app/pokemon.service.ts
+++ projectpokemon/src/app/pokemon.service.ts
@@ -7,42 +7,50 @@ import { Pokemon } from './pokemon';
 
 @Injectable()
 export class PokemonService {
-	private pokeApiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=4';
+  private pokeApiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=4';
 
   constructor(
-  	private http: Http
-	) { }
+    private http: Http
+  ) { }
 
   getPokemons(): Promise<Array<Pokemon>> {
-  	return this.http
-  		.get(this.pokeApiUrl)
-  		.toPromise()
-  		.then((pokemonsResponse) => {
-        let pokemons = Object.assign([], pokemonsResponse.json().results);
-        pokemons.map(pokemon => {
-          this.getPokemon(pokemon.url)
-            .then((pokemonResponse) => {
-              pokemon.id = pokemonResponse.id;
-            })
-        })
-        return pokemons as Pokemon[];
-  		})
-  		.catch(this.handleError);
+    return new Promise((resolve, reject) => {
+      this.http
+      .get(this.pokeApiUrl)
+      .toPromise()
+      .then((pokemonsResponse) => {
+        const pokemons = Object.assign([], pokemonsResponse.json().results)
+        const pokemonPromises = pokemons.map( pokeObj =>
+          this.getPokemon(pokeObj.url)
+        )
+                                             
+        Promise.all(pokemonPromises)
+          .then((values) => {
+              resolve(values)
+          })
+          .catch(reject)
+       
+      })
+      .catch(this.handleError);
+    })
+    
   }
 
   getPokemon(pokemonUrl: string): Promise<Pokemon> {
-    return this.http
-    	.get(pokemonUrl)
-    	.toPromise()
-    	.then((response) => {
-    		return response.json();
-    	})
-    	.catch(this.handleError);
+    return new Promise((resolve, reject) => {
+      this.http
+        .get(pokemonUrl)
+        .toPromise()
+        .then((response) => {
+          resolve(response.json())
+        })
+        .catch(reject);
+    })
   }
 
   private handleError(error: any): Promise<any> {
     console.error('An error occurred', error);
     return Promise.reject(error.message || error);
-	}
+  }
 
 }
